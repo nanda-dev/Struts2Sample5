83cf86fae682d8a187ab3fd94ee85b0857b82b53
iamranga <rangarajan.ramadass@cognizant.com>
2020-08-18 15:50:41 +0530

    initial commit

---
+++ src/main/java/org/integrallis/bookstore/service/helper/FileUtility.java
@@ -0,0 +1,19 @@
+package org.integrallis.bookstore.service.helper;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileUtility {
+
+
+
+    void generateReportFile(String content) throws IOException {
+
+        FileWriter fileWriter = new FileWriter("~/DailyReports/Report.csv");
+        fileWriter.write(content);
+        fileWriter.close();
+    }
+
+}
+
+
